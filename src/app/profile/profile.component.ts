61a05bca4f5eaa2f6f3fb7152d9f4b154cb9be0d
Xu Wang <834166254@qq.com>
2021-05-04 21:16:26 +0800

    Add nav links and animations

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,19 @@
+import { animateChild, query, transition, trigger } from "@angular/animations";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-profile",
   templateUrl: "./profile.component.html",
   styleUrls: ["./profile.component.scss"],
+  animations: [
+    trigger("animate", [
+      transition("* => *", [
+        query(":enter, :leave", [animateChild()], {
+          optional: true,
+        }),
+      ]),
+    ]),
+  ],
 })
 export class ProfileComponent implements OnInit {
   showNavigationMenu = false;
