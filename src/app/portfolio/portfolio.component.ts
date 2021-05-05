aeeac6abc617efa9b1101f0ab7b004c5bc253204
Xu Wang <834166254@qq.com>
2021-05-05 13:06:56 +0800

    Update

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostListener, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Component({
@@ -7,12 +7,44 @@ import { Router } from "@angular/router";
   styleUrls: ["./portfolio.component.scss"],
 })
 export class PortfolioComponent implements OnInit {
-  typingText = "Hey, sort my projects by clicking the categories.";
+  private typingText = "Hey, sort my projects by clicking the categories.";
+  private setFixedPosition = false;
+
+  @ViewChild("portfolioSelectionArea", { static: true })
+  portfolioSelectionAreaRef!: any;
+  @ViewChild("portfolioHeaderArea", { static: true })
+  portfolioHeaderAreaRef!: any;
+  @ViewChild("portfolioArea", { static: true })
+  portfolioAreaRef!: any;
+
   constructor(private router: Router) {}
 
   ngOnInit() {}
 
   onBackBtnClick() {
     this.router.navigateByUrl("/");
+  }
+
+  @HostListener("window:scroll")
+  onWindowScroll() {
+    // console.log(window.scrollY);
+    // const portfolioSelectionArea: HTMLElement = this.portfolioSelectionAreaRef
+    //   .nativeElement;
+    // const portfolioHeaderArea: HTMLElement = this.portfolioHeaderAreaRef
+    //   .nativeElement;
+    const portfolioArea: HTMLElement = this.portfolioAreaRef.nativeElement;
+
+    console.log(
+      // portfolioSelectionArea.offsetTop,
+      // portfolioHeaderArea.scrollHeight,
+      window.scrollY,
+      portfolioArea.offsetTop
+    );
+
+    if (window.scrollY > portfolioArea.offsetTop) {
+      this.setFixedPosition = true;
+    } else {
+      this.setFixedPosition = false;
+    }
   }
 }
